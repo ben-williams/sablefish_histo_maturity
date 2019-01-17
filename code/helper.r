e26ef075f0146d40eab92b779398a2522561988a
Ben <ben.williams@alaska.gov>
2019-01-17 09:35:15 -0900

    init commit

--- code/helper.r
+++ code/helper.r
@@ -1,5 +1,8 @@
+# load these packages...
+
 library(tidyverse)
 library(lubridate)
 library(FNGr) # devtools::install_github("ben-williams/FNGr")
 library(mgcv)
+library(readxl)
 theme_set(theme_sleek())
